30ab0b923a2000a0b43731478f0f30e5998a5c24
MattiaR <rovitomattia91@gmail.com>
2021-06-12 09:48:47 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//TODO STEP 1 Un alert() espone 5 numeri generati casualmente.
+
+//TODO STEP 2 Dopo che l'utente ha cliccato ok sull'alert, ha 30 secondi per tenerseli a mente.  
+
+//TODO STEP 3 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+//TODO STEP 4 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO STEP BONUS controllare che i numeri generati casualmente siano univoci (non ci siano doppioni) ed anche i numeri che l'utente inserisce non devono già essere stati scritti precedentemente.
